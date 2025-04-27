cd632fa0f000045e41467a96891d75cdf7a39fe6
alima987 <alima1996b@gmail.com>
2025-04-27 11:43:23 +0500

    feat: add goodbye and command

--- src/app.js
+++ src/app.js
@@ -1,4 +1,32 @@
-const app = () => {
-    
+import os from 'os'; 
+import path from 'node:path';
+import { cwd }from 'node:process';
+import readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+const rl = readline.createInterface({ input, output });
+export const app = async (username) => {
+    const homedir = os.homedir()
+    console.log(`You are currently in ${homedir}`); 
+    const goodbye = () => {
+        console.log(`Thank you for using File Manager,  ${username}, goodbye!`); 
+    }
+    const command = await rl.question('Enter your command: ');
+      if (command.trim() === '.exit') {
+        exitProcess()
+      } else { 
+        console.log(`Answer: ${command}`);
+      }
+    process.on('exit', () => {
+        goodbye()
+    });
+    process.on('SIGINT', () => {
+        exitProcess()
+    })
+    function exitProcess() {
+        goodbye() 
+        rl.close();
+        process.exit()
+    }
 }
-app()
+
+app();
--- src/greeting.js
+++ src/greeting.js
@@ -1,12 +1,13 @@
-
-const greeting = () => {
-    const args = process.argv.slice(2)
+import { app } from "./app.js"
+const args = process.argv.slice(2)
     const filterArgs = args.findIndex((el) => el.startsWith("--username="))
-    let username
+    let username = "User";
     if (filterArgs !== -1) {
         username = args[filterArgs].split('=')[1]
     }
+const greeting = () => {
     const greet = `Welcome to the File Manager, ${username}!`
     console.log(greet)
 }
 greeting()
+app(username)
